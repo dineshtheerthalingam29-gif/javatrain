3dfe55f30a3d70f113f166950908ec2350f8c64d
dinesh <dineshtheerthalingam29@gmail.com>
2026-06-30 15:45:02 +0530

    Initial

--- day1/prob2.java
+++ day1/prob2.java
@@ -1,7 +1,12 @@
-public class prob1 {
+package day1;
+
+public class prob2 {
+    
     
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
     }
+
+
 
 }
