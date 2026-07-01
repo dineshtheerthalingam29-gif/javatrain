0eb74fd2a015af429b87ff4fb36cb262a4f22482
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-01 12:25:48 +0530

    Initial

--- day1/problem9.java
+++ day1/problem9.java
@@ -1,11 +1,7 @@
-public class problem9 {
-
+class Student {
     String name = "Harini";
     public static void main(String[] args) {
         Student s = new Student();
         System.out.println(s.name);
     }
 }
-
-
-
