5c204f0c0807d5377cbe5150a2855addd61ea749
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-01 11:34:46 +0530

    Initil

---
+++ day1/problem9.java
@@ -0,0 +1,9 @@
+public class problem9 {
+    c
+    String name = "Harini";
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.name);
+    }
+
+}
