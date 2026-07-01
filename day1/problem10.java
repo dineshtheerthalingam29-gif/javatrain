df5e1c932f88024fc1525e6c9784065c7d01345d
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-01 11:36:53 +0530

    Initil

---
+++ day1/problem10.java
@@ -0,0 +1,10 @@
+public class problem10 {
+  
+    void show() {
+        System.out.println("Method Called");
+    }
+    public static void main(String[] args) {
+        new MethodDemo().show();
+    }
+
+}
