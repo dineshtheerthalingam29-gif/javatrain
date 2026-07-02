8ec767df58debb7b03ea1770eaa6f24ee6ac6ef2
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 15:34:45 +0530

    Initial

---
+++ day5/problem11.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class problem11 {
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double c = sc.nextDouble();
+
+        double f = (c * 9 / 5) + 32;
+
+        System.out.println("Fahrenheit = " + f);
+    }
+}
+
