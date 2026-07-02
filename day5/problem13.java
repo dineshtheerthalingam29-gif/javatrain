8b27423d92b734d6904b3d2365d24559ceace50a
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 15:39:01 +0530

    Initial

---
+++ day5/problem13.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class problem13 {
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+
+        System.out.println("ASCII Value = " + (int) ch);
+    }
+}
