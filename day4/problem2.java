2ce363ca60891a4af587aff19e8ec0f1eefa91a6
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 14:10:23 +0530

    Initial

---
+++ day4/problem2.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class problem2 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int value = ch;
+
+        System.out.println(value);
+    }
+}
+
