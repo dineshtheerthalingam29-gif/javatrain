dc5f4a95ffa54c0b80a886a9a0192c5ef2818e8c
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 14:18:45 +0530

    Initial

---
+++ day4/problem8.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class problem8 {
+    public static void main(String[] args) {
+        int num = 120;
+        byte b = (byte) num;
+
+        System.out.println(b);
+    }
+}
+
