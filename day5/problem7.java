659d1bfae25ffd311b3a712e1d2294b07fa243f8
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 15:29:38 +0530

    Initial

---
+++ day5/problem7.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class problem7 {
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter float value: ");
+        float num = sc.nextFloat();
+
+        System.out.println("You entered: " + num);
+    }
+}
+
