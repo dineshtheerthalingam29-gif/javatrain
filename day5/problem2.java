cb6ec7f429e125e8fb9d9878ecef2818fadfcb87
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 15:21:14 +0530

    Initial

---
+++ day5/problem2.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class problem2 {
+    import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
+}
