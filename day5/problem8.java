278d9661325994f0a3050c8f9c89961fa8f65ce6
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 15:30:48 +0530

    Initial

---
+++ day5/problem8.java
@@ -0,0 +1,14 @@
+package day5;
+
+public class problem8 {
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter true or false: ");
+        boolean value = sc.nextBoolean();
+
+        System.out.println("Value = " + value);
+    }
+}
