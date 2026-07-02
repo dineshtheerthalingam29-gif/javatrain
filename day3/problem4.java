d7a91c11400c738a05ccbaad5d0abd95fc88ee4d
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 12:28:48 +0530

    Initial

---
+++ day3/problem4.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class problem4 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+        System.out.println(a);
+        a -= 3;
+        System.out.println(a);
+    }
+}
+
