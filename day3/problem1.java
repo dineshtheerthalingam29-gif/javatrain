c6ba2c9b1e48d454e8023a24dbfd580c39852b3d
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 12:20:27 +0530

    Initial

--- day3/problem1.java
+++ day3/problem1.java
@@ -1,12 +1,14 @@
-public package day3;
+package day3;
 
-class problem1 {
+public class problem1 {
 
     public static void main(String[] args) {
-        double num = 10.8;
-        int value = (int) num;
-
-        System.out.println(value);
+        int a = 10, b = 5;
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Modulus = " + (a % b));
     }
 }
 
