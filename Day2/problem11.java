3637448fd2fea60caecaa615d3d54496c4dae036
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 11:53:46 +0530

    Initial

---
+++ Day2/problem11.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class problem11 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = ch;
+
+        System.out.println(ascii);
+    }
+}
+
