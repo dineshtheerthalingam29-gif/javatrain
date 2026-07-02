bb640a9b0e93d0c0ae082af6d5f4770b465d2f17
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 14:01:46 +0530

    Initial

---
+++ day3/problem12.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class problem12 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println(max);
+    }
+}
+
