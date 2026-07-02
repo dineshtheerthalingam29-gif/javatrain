128484efb9ed9f0ca8900f4e266d7dcf3fe0d2a0
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 12:05:26 +0530

    Initial

---
+++ day3/problem1.java
@@ -0,0 +1,12 @@
+public package day3;
+
+class problem1 {
+
+    public static void main(String[] args) {
+        double num = 10.8;
+        int value = (int) num;
+
+        System.out.println(value);
+    }
+}
+
