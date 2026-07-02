3acfcdef1c306f401fbf2a6741a19e449df79c15
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-02 14:20:01 +0530

    Initial

---
+++ day4/problem9.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class problem9 {
+    public static void main(String[] args) {
+        double d = 25.67;
+        float f = (float) d;
+
+        System.out.println(f);
+    }
+}
