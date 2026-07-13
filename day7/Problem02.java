2348fca002ab02852956b29183a20e663359e0f6
dinesh <dineshtheerthalingam29@gmail.com>
2026-07-13 14:23:25 +0530

    Initial

--- day7/Problem02.java
+++ day7/Problem02.java
@@ -5,7 +5,7 @@ public class Problem02 {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 sum += arr[i][j];
-                System.out.print(arr[i][j] + " ");
+                System.git out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
